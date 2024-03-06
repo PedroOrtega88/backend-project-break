ddfd46fb761208a63da223eb75086d73050ce829
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-03-06 20:38:12 +0100

    -a

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -8,22 +8,16 @@ router.get('/products', productController.showProducts);
 // Ruta para mostrar el detalle de un producto por su ID
 router.get('/products/:productId', productController.showProductById);
 
-// Ruta para mostrar el formulario para crear un nuevo producto
-router.get('/dashboard/new', productController.getNewProductForm);
-
-// Ruta para manejar la creación de un nuevo producto
-router.post('/dashboard/new', productController.createProduct);
-
-// Ruta para crear un nuevo producto
-router.post('/dashboard', productController.createProduct);
+// Ruta para mostrar el formulario para crear un nuevo producto y manejar la creación
+router.route('/dashboard/new')
+  .get(productController.showNewProductForm)
+  .post(productController.createProduct);
 
 // Ruta para mostrar el formulario de edición de un producto por su ID
 router.get('/dashboard/:productId/edit', productController.showEditProductForm);
 
 // Ruta para actualizar un producto por su ID
 router.put('/dashboard/:productId/update', productController.updateProduct);
-
-
 
 // Ruta para mostrar el detalle de un producto en el dashboard
 router.get('/dashboard/:productId', productController.showProductInDashboard);
