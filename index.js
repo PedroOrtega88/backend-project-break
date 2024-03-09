4df3e035ea060fdbeebf81db43bb817a098f4172
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-03-09 20:20:20 +0100

    -a

--- index.js
+++ index.js
@@ -6,22 +6,20 @@ const productRoutes = require('./routes/ProductRoutes');
 const connectDB = require('./config/db'); // Importar connectDB
 const Product = require('./models/Product');
 
-// Configuración de variables de entorno
 dotenv.config({ path: './.env' });
 
-// Inicialización de la aplicación Express
 const app = express();
 
 
-// Middleware para manejar datos del formulario
+// Middleware 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.set('pagina', './pagina'); // Directorio donde se encuentran tus vistas
+app.set('pagina', './pagina'); 
 app.use(methodOverride('_method'));
 
 
-// Conexión a la base de datos
+//conectar con la base de datos
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -31,25 +29,25 @@ mongoose.connect(process.env.MONGO_URI, {
   console.error('Error al conectar a la base de datos:', err);
   process.exit(1);
 });
+
 //CSS
 app.use(express.static('public'));
 
-// Configuración de las rutas
+
 app.use('/', productRoutes);
 
-// Middleware para la barra de menú
+// Middleware nav.menu 
 app.use((req, res, next) => {
   res.locals.categories = ['Camisetas', 'Pantalones', 'Zapatos', 'Accesorios'];
   next();
 });
 
-// Manejador para la ruta raíz
+
 app.get('/', async (req, res) => {
   try {
-    // Obtener todos los productos
-    const products = await Product.find();
+      const products = await Product.find();
 
-    // Generar la lista de productos en HTML
+    // Lista de productos en html
     const productsList = products.map(product => `
       <li>
       <img src="${product.image}" alt="${product.name}">
@@ -60,7 +58,7 @@ app.get('/', async (req, res) => {
       </li>
     `).join('');
 
-    // Renderizar la página con la lista de productos
+    // enviar la lista de productos
     res.send(`
       <!DOCTYPE html>
       <html lang="es">
@@ -93,12 +91,12 @@ app.get('/', async (req, res) => {
   }
 });
 
-// Manejador para la ruta dashboard
+// Dashboard
 app.get('/dashboard/', async (req, res) => {
-  // Obtener todos los productos
+
   const products = await Product.find();
 
-  // Generar la lista de productos en HTML
+  // Productos
   const productsList = products.map(product => `
     <li>
     <img src="${product.image}" alt="${product.name}">
@@ -139,7 +137,6 @@ app.get('/dashboard/', async (req, res) => {
   `);
 });
 
-// Inicio del servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
